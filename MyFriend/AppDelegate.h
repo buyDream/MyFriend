83e6e07f0341794aa23bddad8b07da95485b9772
Refrain <13239997400@163.com>
2017-06-11 12:13:24 +0800

    Initial Commit

---
+++ MyFriend/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFriend
+//
+//  Created by Refraining on 2017/6/11.
+//  Copyright © 2017年 Refraining. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyFriend/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyFriend
+//
+//  Created by Refraining on 2017/6/11.
+//  Copyright © 2017年 Refraining. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
